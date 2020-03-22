d01ceab75a75517520e2ecc7ff9f0594cde81ffd
natasha17 <17061990@inbox.ru>
2020-03-22 12:09:54 +0300

    Updates

---
+++ precache-manifest.e2e4221a3f783c9dc2720cf11aef22b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41661cdc7bf71612f89b8c25fce19a05",
+    "url": "/Simple_Counter_Redux/index.html"
+  },
+  {
+    "revision": "85520e5b7f38f0f0510e",
+    "url": "/Simple_Counter_Redux/static/css/main.5428c441.chunk.css"
+  },
+  {
+    "revision": "685f442d512e2fdc9d0b",
+    "url": "/Simple_Counter_Redux/static/js/2.ddb48584.chunk.js"
+  },
+  {
+    "revision": "85520e5b7f38f0f0510e",
+    "url": "/Simple_Counter_Redux/static/js/main.e9ac9683.chunk.js"
+  },
+  {
+    "revision": "5f76e4091d3baaee9faa",
+    "url": "/Simple_Counter_Redux/static/js/runtime~main.d148d1ab.js"
+  },
+  {
+    "revision": "53eab731e86dddc596841601a3f8f977",
+    "url": "/Simple_Counter_Redux/static/media/back.53eab731.jpg"
+  }
+]);
